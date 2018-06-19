7d3bb06718595a7a1b56611b6ebd23ca49f81fe9
Zach Wong <zrwong2@linux-15.ews.illinois.edu>
2018-06-18 21:02:27 -0500

    updated version of lab1b

--- lab1/lab1b/src/print_hex.asm
+++ lab1/lab1b/src/print_hex.asm
@@ -12,12 +12,52 @@
 MAIN
 
 ; INSERT CODE HERE!
+				AND R0, R0, #0
+				AND R5, R5, #0	
+				AND R1, R1, #0
+				AND R4, R4, #0
+				AND R2, R2, #0
 
+				LD R3, HEX
+				
+				ADD R2, R2, #4
+LOOP2			ADD R2, R2, #0			
+				BRnz DONE
+	
+				ADD R1, R1, #4			
+LOOP1			ADD R5, R5, R5			
+				ADD R1, R1, #0
+				BRnz DIGIT_CHECK
+				ADD R3, R3, #0
+				BRzp L_SHIFT
+				ADD R5, R5, #1
+
+L_SHIFT			ADD R3, R3, R3
+				ADD R1, R1, #-1
+				BRnzp LOOP1
+				
+						
+DIGIT_CHECK		ADD R4, R5, #0
+				ADD R4, R4, #-9
+				BRnz NUMBER
+				LD R0, ASCII_LET
+				ADD R0, R5, R0
+				BRnzp #2
+NUMBER			LD R0, ASCII_NUM
+				ADD R0, R5, R0 
+				OUT
+
+				AND R5, R5, #0
+				AND R4, R4, #0
+				AND R0, R0, #0
+				ADD R2, R2, #-1
+				BRnzp LOOP2
 DONE
-    HALT
+   				HALT
 
 ; Data
 ASCII_NUM .FILL x0030
 ASCII_LET .FILL x0041
+HEX		  .FILL x0123
 
 .END
