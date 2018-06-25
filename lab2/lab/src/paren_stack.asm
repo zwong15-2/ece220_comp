1ccc965ac217f74f225af3cfe6aa7a14faa9c143
Zach Wong <zrwong2@linux-15.ews.illinois.edu>
2018-06-24 20:39:36 -0500

    lab completed

--- lab2/lab/src/paren_stack.asm
+++ lab2/lab/src/paren_stack.asm
@@ -14,11 +14,61 @@
 MAIN
 
 ; INSERT CODE HERE!
-	
-HALT_MAIN
-	HALT
+			AND R1, R1, #0 ;Initialzation
+			AND R2, R2, #0
+			AND R3, R3, #0
+			AND R4, R4, #0
+			AND R5, R5, #0
+			AND R6, R6, #0
+
+AGAIN		LD R1, SPACE
+			LD R2, NEG_OPEN
+			LD R3, NEG_CLOSE
+			NOT R1, R1
+			ADD R1, R1, #1
+		
+
+			GETC
+			ADD R1, R0, R1
+			BRz AGAIN
+			ADD R2, R0, R2
+			BRz CHECK1
+			ADD R3, R0, R3
+			BRz CHECK2
+			JSR IS_BALANCED2
+			ADD R4, R4, #0
+			BRzp #1
+			ADD R6, R6, #-2
+			BRnzp DONE
+
+CHECK1      JSR PUSH
+			ADD R5, R5, #0
+			BRp FAIL
+			BRnz PRINT
+
+CHECK2	    JSR IS_BALANCED
+			ADD R5, R5, #0
+			BRp FAIL
+			ADD R6, R6, #0
+			BRz FAIL			
+
+PRINT		OUT
+			AND R1, R1, #0
+			AND R2, R2, #0
+			AND R3, R3, #0
+			AND R4, R4, #0
+            BRnzp AGAIN
+
+FAIL        ADD R6, R6, #-1			
+
+ERROR		OUT
+DONE		HALT_MAIN
+			
+			HALT
 		
 ; Data
+CHECK_BASE  .FILL x-3FFF
+JUMP        .FILL x0000
 SPACE	    .FILL x0020
 NEW_LINE    .FILL x000A
 CHAR_RETURN .FILL x000D
@@ -27,12 +77,46 @@ CHAR_RETURN .FILL x000D
 ;   Description: checks if a string is balanced or not
 ;   Inputs: R0 - input character to push/pop
 ;   Outputs: R6 - integer indicating balanceness
-IS_BALANCED
+
 	
 ; INSERT CODE HERE!
 
+IS_BALANCED 
+				
+				ST R7, JUMP
+				JSR POP
+				ADD R5, R5, #0
+				BRp NON_MATCH
+				NOT R6, R6
+				ADD R6, R6, #1
+				ADD R6, R0, R6
+				BRnz NON_MATCH
+				BRp DONE2
+
+
+
+IS_BALANCED2    ST R7, JUMP
+				LD R4, CHECK_BASE
+				LD R3, STACK_TOP
+				ADD R4, R4, #-1
+				ADD R4, R3, R4
+				BRnp NON_MATCH2
+				ADD R6, R6, #0
+				BRnzp #3
+NON_MATCH		AND R6, R6, #0
+				
+	
+
+NON_MATCH2		ADD R6, R6, #0
+				
+DONE2			LD R7, JUMP			
+				RET
+				
+			
+
 ; Data
 NEG_OPEN .FILL xFFD8
+NEG_CLOSE .FILL xFFD7
 
 ; PUSH
 ;   Description: Pushes a charcter unto the stack
@@ -47,7 +131,7 @@ PUSH
 	LD R3, STACK_END	;
 	LD R4, STACk_TOP	;
 	ADD R3, R3, #-1		;
-	NOT R3, R3		;
+	NOT R3, R3		    ;
 	ADD R3, R3, #1		;
 	ADD R3, R3, R4		;
 	BRz OVERFLOW		;stack is full
@@ -80,7 +164,7 @@ POP
 	AND R5, R5, #0		;clear R5
 	LD R3, STACK_START	;
 	LD R4, STACK_TOP	;
-	NOT R3, R3		;
+	NOT R3, R3		    ;
 	ADD R3, R3, #1		;
 	ADD R3, R3, R4		;
 	BRz UNDERFLOW		;
