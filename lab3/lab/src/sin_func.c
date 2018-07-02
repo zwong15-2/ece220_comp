bae5c32fcdf8e13f52448683a24e101ad543c94e
Zach Wong <zrwong2@linux-17.ews.illinois.edu>
2018-07-01 19:09:20 -0500

    lab 3

--- lab3/lab/src/sin_func.c
+++ lab3/lab/src/sin_func.c
@@ -14,17 +14,37 @@
 int main()
 {
     // Declare variables
+    float omega1, omega2;
+    int n, i;
+    float x_i;
+    float f_x;
 
     // Prompt user for input
+    printf("Enter the values of n, omega1, and omega2 in that order: \n");
+    
 
     // Get user input
+    scanf("%d", &n);
+    scanf("%f", &omega1);
+    scanf("%f", &omega2);
+   
+
 
     // Compute function output
     /* for i from 0 to n-1
      *     compute and print xi and f(xi)
      *     use sin() function from math.h
      */
+    
+    for(i = 0; i <= (n-1); i++){
 
+    x_i = (i*(M_PI))/n;
+
+    f_x = sin(omega1 * x_i) + (0.5)*(sin(omega2 * x_i));
+
+    printf("(%f, %f)\n", x_i, f_x);
+
+    }
     return 0;
 }
 
