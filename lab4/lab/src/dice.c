a99062adce20608d16c37c15ad67381b6c8a11ad
Wong <zrwong2@linux-19.ews.illinois.edu>
2018-07-09 18:53:02 -0500

    lab 4

--- lab4/lab/src/dice.c
+++ lab4/lab/src/dice.c
@@ -1,5 +1,29 @@
 //Function for generating three d6 rolls
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "dice.h"
+
 void roll_three(int* one, int* two, int* three){
 
+int i;
+
+int n = 3;
+
+for( i = 0; i < n; i++)
+   {
+     if(i == 0)
+       { 
+         *one = rand() % (6 + 1 - 1) + 1;
+       }    
+     if(i == 1)
+       {
+        *two = rand() % (6 + 1 - 1) + 1;
+       }  
+     if(i == 2)
+       {
+        *three = rand() % (6 + 1 - 1) +1;
+       }     
+          
+   }
 }
