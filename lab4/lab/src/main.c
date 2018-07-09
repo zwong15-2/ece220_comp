42f5ff61d89213e82e517f099edf973f650f30be
Wong <zrwong2@linux-19.ews.illinois.edu>
2018-07-09 18:53:19 -0500

    lab 4

--- lab4/lab/src/main.c
+++ lab4/lab/src/main.c
@@ -8,4 +8,41 @@
  * Otherwise print "\n"
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dice.h"
 
+int main () {
+  int s;
+  int num_one;
+  int num_two;
+  int num_three;
+
+  printf("Enter Seed: \n");
+
+  scanf("%d", &s);
+  
+  srand(s);
+  
+  roll_three(&num_one, &num_two, &num_three); 
+  
+  if(num_one == num_two && num_two == num_three)
+     {
+       printf("%d %d %d Triple!\n", num_one, num_two, num_three);
+     }  
+ 
+   else if(num_one == num_two || num_one == num_three || num_two == num_three)
+     { 
+       printf("%d %d %d Double!\n", num_one, num_two, num_three);
+     }
+ 
+  else
+     {
+       printf("%d %d %d\n", num_one,  num_two,  num_three);
+     }
+
+return 0;
+
+}
+ 
+  
