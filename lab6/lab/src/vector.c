6bc9968aeafe12d308f2303d008594a2a9d3c5ec
Wong <zrwong2@linux-a3.ews.illinois.edu>
2018-07-23 18:13:31 -0500

    lab6

--- lab6/lab/src/vector.c
+++ lab6/lab/src/vector.c
@@ -4,25 +4,75 @@
 
 vector_t * createVector(int initialSize)
 {
+     vector_t * vector = malloc(sizeof(vector_t));
+     vector->array = (int *)malloc(initialSize*sizeof(int*));
+     vector->maxSize = initialSize;
+     vector->size = 0;
+
+ return vector;
 }
 
 void destroyVector(vector_t * vector)
 {
+free(vector->array);
+free(vector);
 }
 
 void resize(vector_t * vector)
 {
+ 
+vector->array = (int *)realloc(vector->array,(vector->maxSize*2)*sizeof(int*));
+vector->maxSize = ((vector->maxSize) * 2 );
 }
 
 void push_back(vector_t * vector, int element)
 {
+int i;
+int value = element;
+int counter;
+for(i = 0; i < vector->size; i++)
+   {
+     if(vector->size == vector->maxSize)
+       {
+        vector->array = (int *)realloc(vector->array, (vector->maxSize*2)*sizeof(int*));
+        vector->array[i] = value;
+        counter++;
+        vector->size = counter;
+        vector->maxSize = (vector->maxSize * 2);
+       }
+     else
+       {
+         vector->array[i] = value;
+         counter++;
+         vector->size = counter;
+       }
+    }
 }
 
 int pop_back(vector_t * vector)
 {
+ int i;
+ if(vector->size == 0)
+ {
+  return 0;
+ }
+ for(i = 0; (vector + i)->array != '\0'; i++)
+ {
+
+ }
+ vector->size -= 1;
+ return vector->array[--i];
+
 }
 
 int access(vector_t * vector, int index)
 {
+ if((index > vector->maxSize) || (index < 0))
+   {
+    return 0;
+   }
+ else
+   {
+    return vector->array[index];
+   }
 }
-
