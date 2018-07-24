bad547bba21fe7e6371ec0199de1bf362223f640
Wong <zrwong2@linux-19.ews.illinois.edu>
2018-07-23 21:56:30 -0500

    mp6 done

--- mp6/maze.c
+++ mp6/maze.c
@@ -1,3 +1,11 @@
+/*In this mp we built a program that will solve a solvable maze and
+ * return unsolvable if the maze cannot be solved. These four functions
+ * are crucial for the program to function properly. The first function createMaze
+ * creates the maze structure filling in the given cells based off the maze file.
+ * destoryMaze frees up all the allocated memory and printMaze will print the maze
+ * cells to the console. The last function will then solve the maze if it is possible 
+ * or inform the user the maze is unsolvable.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "maze.h"
@@ -10,10 +18,56 @@
  * RETURN:       A filled maze structure that represents the contents of the input file
  * SIDE EFFECTS: None
  */
-maze_t * createMaze(char * fileName)
-{
-    // Your code here. Make sure to replace following line with your own code.
-    return NULL;
+maze_t * createMaze(char * fileName){
+    //initialization of variables
+    int row;
+    int col;
+    int width;
+    int height;
+    char x;
+    FILE* maze_file;
+
+
+    maze_t * maze = malloc(sizeof(maze_t)); 	                //allocate memory for structure
+
+    maze_file = fopen((char*)fileName, "r"); 			//opens maze file and receives width and height values for maze
+
+    fscanf(maze_file, "%d %d", 	&width, &height);
+
+    maze->width = width;         //sets width and height parameters for maze structure
+    maze->height = height;
+
+    maze->cells = (char **) malloc((height) * sizeof(char *)); 	//allocates memory for cells within the maze
+
+    for(row = 0; row < (height); row++){
+        maze->cells[row] = (char *) malloc((width-1) * sizeof(char *));
+    }
+
+    for(row = 0; row < (height); row++){ 			//gets the characters from the file and stores into the cells of the maze array
+        for(col = 0; col < (width); col++){
+            x = fgetc(maze_file);
+
+            if(x == '\n'){                              //if newline occurs, get next character
+              x = fgetc(maze_file);
+            }
+
+            if(x == START){ 				//sets parameters for startRow and startColumn for maze structure when START definition is found
+               maze->startRow = row;
+               maze->startColumn = col;
+            }
+
+            if(x == END){                             //sets parameters for endRow and endColumn for maze structure when END definition is found
+               maze->endRow = row;
+               maze->endColumn = col;
+            }
+            maze->cells[row][col] = x;
+             
+        }
+   }
+
+   fclose(maze_file);  //closes file
+          
+return maze; //returns pointer to maze structure
 }
 
 /*
@@ -25,7 +79,12 @@ maze_t * createMaze(char * fileName)
  */
 void destroyMaze(maze_t * maze)
 {
-    // Your code here.
+	int i;   //initialize varible
+	for(i = 0; i < maze->height; i++){ //loop through cells free up the allocated columns
+    		free(maze->cells[i]);	
+   	}
+        free(maze->cells);  //free allocated rows from cells
+	free(maze);        //free allocated maze structure
 }
 
 /*
@@ -37,9 +96,20 @@ void destroyMaze(maze_t * maze)
  * RETURN:       None
  * SIDE EFFECTS: Prints the maze to the console
  */
-void printMaze(maze_t * maze)
-{
-    // Your code here.
+
+void printMaze(maze_t * maze){
+    int i;                 //initialization of variables
+    int j;
+    int width = maze->width; //setting width and height variables to width and height values
+    int height = maze->height;
+
+    for(i = 0; i < height; i++){   //loop through cells and print them to console
+    	for(j = 0; j < width; j++){
+             printf("%c", maze->cells[i][j]);
+        }
+        printf("\n");
+   }
+
 }
 
 /*
@@ -51,8 +121,58 @@ void printMaze(maze_t * maze)
  * RETURNS:              0 if the maze is unsolvable, 1 if it is solved
  * SIDE EFFECTS:         Marks maze cells as visited or part of the solution path
  */ 
-int solveMazeDFS(maze_t * maze, int col, int row)
-{
-    // Your code here. Make sure to replace following line with your own code.
-    return 0;
+int solveMazeDFS(maze_t * maze, int col, int row){
+if(col < 0 || row < 0 || col >= maze->width || row >= maze->height){	//if position lies outside of maze, return 0;
+	return 0;
+}
+
+switch(maze->cells[row][col]){    
+    case WALL:				//if position isn't start, then check if its empty, if its not, return 0 
+    	return 0;
+    case END:				//if the end of maze is reached, return 0
+	return 1;
+    case START:                         //if in the start position, check the possible pathways from start
+	if((maze->cells[row - 1][col] == PATH) && (maze->cells[row - 2][col] == VISITED)){
+		maze->cells[row - 1][col] = VISITED;		
+	}
+	if((maze->cells[row + 1][col] == PATH) && (maze->cells[row + 2][col] == VISITED)){
+		maze->cells[row + 1][col] = VISITED;		
+	}
+	if((maze->cells[row][col - 1] == PATH) && (maze->cells[row][col - 2] == VISITED)){
+		maze->cells[row][col - 1] = VISITED;		
+	}
+	if((maze->cells[row][col + 1] == PATH)  && (maze->cells[row][col + 2] == VISITED)){
+		maze->cells[row][col + 1] = VISITED;		
+	}
+	break;
+    case VISITED:              //if position is already visited, return 0
+	return 0;
+    case PATH:                //if position is already a part of the pathway, return 0
+	return 0;
+    case EMPTY:               //if position is in an empty space, mark it as a pathway
+	maze->cells[row][col] = PATH;	
+    	break;
+}
+//Recursive Cases
+if(solveMazeDFS(maze, col - 1, row) == 1){				//if solveMaze to left returns 1, return 1
+	return 1;
+}
+else if(solveMazeDFS(maze, col, row - 1) == 1){				//if solveMaze to down returns 1, return 1
+	return 1;
+}  
+else if(solveMazeDFS(maze, col + 1, row) == 1){				//if solveMaze going right returns 1, return 1
+	return 1;
+}
+else if(solveMazeDFS(maze, col, row + 1) == 1){				//if solveMaze going up returns 1, return 1
+	return 1;
+}
+else{                                                                   //marks the cells visited if the cell is not the starting spot
+	if((maze->cells[row][col] != START)){
+		maze->cells[row][col] = VISITED;
+	}
+	return 0;
+}
+
+	
+ 	     	
 }
