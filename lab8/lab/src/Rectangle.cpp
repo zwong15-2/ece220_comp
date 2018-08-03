d7c9fafc7f3efa0a96ae8a45c4e2bdc22dd1ef08
Wong <zrwong2@linux-a3.ews.illinois.edu>
2018-08-02 20:56:51 -0500

    Lab8 Complete

--- lab8/lab/src/Rectangle.cpp
+++ lab8/lab/src/Rectangle.cpp
@@ -3,12 +3,15 @@
 //Empty Constructor sets instantiates a rectangle with length and width set as 0
 Rectangle::Rectangle(){
 //INSERT YOUR CODE HERE:
-
+length = 0;
+width = 0;
 }
 
 //Standard Constructor sets instantiates a rectangle with length and width set as input values
 Rectangle::Rectangle(int input_length, int input_width){
 //INSERT YOUR CODE HERE:
+length = input_length;
+width = input_width;
 
 }
 
@@ -16,39 +19,46 @@ Rectangle::Rectangle(int input_length, int input_width){
 //Instantiate a rectangle with length R1 = length R2 and width R1 = width R2
 Rectangle::Rectangle( const Rectangle& other ){
 //INSERT YOUR CODE HERE:
-
+length = other.length;
+width = other.width;
 }
 
 //Setter and Getter functions
 void Rectangle::set_length(int input_length){
 //INSERT YOUR CODE HERE:
+length = input_length;
 
 }
 
 void Rectangle::set_width(int input_width){
 //INSERT YOUR CODE HERE:
+width = input_width;
 
 }
 
 int Rectangle::get_length(void) const{
 //INSERT YOUR CODE HERE:
+return length;
 
 }
 
 int Rectangle::get_width(void) const{
 //INSERT YOUR CODE HERE:
+return width;
 
 }
 
 //Calculate Area of rectangle
 int Rectangle::area(void){
 //INSERT YOUR CODE HERE:
+return (length*width);
 
 }
 
 //Calculate Perimeter of rectangle
 int Rectangle::perimeter(void){
 //INSERT YOUR CODE HERE:
+return (2*(length+width));
 
 }
 
@@ -59,6 +69,13 @@ int Rectangle::perimeter(void){
 */
 Rectangle Rectangle::operator + (const Rectangle& other){
 //INSERT YOUR CODE HERE:
+  Rectangle addition;
+
+  addition.length = this->length + other.length;
+  
+  addition.width = this->width + other.width;
+  
+return addition;
 
 }
 
@@ -69,5 +86,8 @@ Rectangle Rectangle::operator + (const Rectangle& other){
 */
 Rectangle Rectangle::operator * (const Rectangle& other){
 //INSERT YOUR CODE HERE:
-
+Rectangle multiplication;
+  multiplication.length = this->length * other.length;
+  multiplication.width = this->width * other.width;
+return multiplication;
 }
